2888f226fc236ffc567f47a5fa24efb94fb52f38
erictc95 <maxerictc95@hotmail.com>
2026-07-06 12:02:23 +0200

    feat: create user entity and application enums

---
+++ backend/src/main/java/com/panhoramix/backend/entity/User.java
@@ -0,0 +1,41 @@
+package com.panhoramix.backend.entity;
+
+
+import com.panhoramix.backend.entity.enums.Role;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String username;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @Column(name = "avatar_url")
+    private String avatarUrl;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+}
