7b13b25a17e60491f54b7c1bb44b202c5b95e72a
erictc95 <maxerictc95@hotmail.com>
2026-07-08 13:49:13 +0200

    feat(frontend): complete user registration flow

---
+++ backend/src/main/java/com/panhoramix/backend/dto/response/LoginResponse.java
@@ -0,0 +1,12 @@
+package com.panhoramix.backend.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class LoginResponse {
+
+    private String token;
+
+}
