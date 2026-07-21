a400ecfee0d1caa1d3141cd2b71f36cbf385f6ed
erictc95 <maxerictc95@hotmail.com>
2026-07-21 11:21:26 +0200

    fix(frontend): visual logos of landing page.

--- frontend/src/pages/Landing/LandingPage.jsx
+++ frontend/src/pages/Landing/LandingPage.jsx
@@ -1,21 +1,58 @@
 import "./LandingPage.css";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 import PHButton from "../../components/common/PHButton";
 
 function LandingPage() {
     return (
         <main className="landing">
 
-            <div className="logo-frame">
+            <div className="frame">
 
-                <span className="corner bottom-left"></span>
-                <span className="corner bottom-right"></span>
+                <div className="logo-frame">
 
-                <h1>PANHORAMIX</h1>
+                    <svg
+                        className="frame-svg"
+                        viewBox="0 0 100 100"
+                        preserveAspectRatio="none"
+                    >
 
-                <p className="subtitle">
-                    Designed for Landscape Cinema
-                </p>
+                        <path
+                            d="M2 18 L2 2 L18 2"
+                            stroke="var(--gold)"
+                            strokeWidth="1"
+                            fill="none"
+                        />
+
+                        <path
+                            d="M82 2 L98 2 L98 18"
+                            stroke="var(--gold)"
+                            strokeWidth="1"
+                            fill="none"
+                        />
+
+                        <path
+                            d="M2 82 L2 98 L18 98"
+                            stroke="var(--gold)"
+                            strokeWidth="1"
+                            fill="none"
+                        />
+
+                        <path
+                            d="M88 98 L98 98 L98 78"
+                            stroke="var(--gold)"
+                            strokeWidth="1"
+                            fill="none"
+                        />
+
+                    </svg>
+
+                    <h1>PANHORAMIX</h1>
+
+                    <p className="subtitle">
+                        Designed for Landscape Cinema
+                    </p>
+
+                </div>
 
             </div>
 
