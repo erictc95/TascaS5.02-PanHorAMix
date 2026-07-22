1cf1a17aeedd9227ad1276bfb107490190ba4bbb
erictc95 <maxerictc95@hotmail.com>
2026-07-22 17:40:08 +0200

    fix: localhost config

--- frontend/src/api/axiosConfig.js
+++ frontend/src/api/axiosConfig.js
@@ -1,15 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-
-    baseURL: "http://localhost:8080/api",
-
+    baseURL: `http://${window.location.hostname}:8080/api`,
     headers: {
-
         "Content-Type": "application/json"
-
     }
-
 });
 
 api.interceptors.request.use(
